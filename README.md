3efdd8c867d3b6cb028d46463782b8f4f925e330
Marcus <marcusbeckerman@protonmail.com>
2020-11-15 14:03:05 -0800

    Added custom image upload for posts

---
+++ README.md
@@ -0,0 +1,11 @@
+# MarcusBeckerman.com
+
+## Purpose
+The purpose of this website is to be a personal blogging website, and also a hub that contains links to my profiles on github, linkedin, etc. I'm going to use this website to post blogs about projects I create, technologies I'm interested in, and whatever else I feel like, really.
+
+## Requirements
+This website was created using a Python 3 backend framework called Flask.
+
+## More info
+Want more info on how I created this website? [Check out my full blog post on it here!](http://marcusbeckerman.com)
+
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect, session, flash
+from flask import Flask, render_template, url_for, request, redirect, session, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 import bcrypt
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/Documents/.my projects/flask-website/personal-blog/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./blog.db'
 app.secret_key = os.urandom(12)
 
 db = SQLAlchemy(app)
@@ -13,6 +14,7 @@ pwhash = b'$2b$15$I8ui9z0gGivDf9sis5wB6O0HElDZGkXldSLcKUyya0asurKRhO9tW'
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(30))
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
     author = db.Column(db.String(20))
@@ -28,7 +30,6 @@ def index():
 @app.route('/post/<int:post_id>')
 def post(post_id):
     post = Blogpost.query.filter_by(id=post_id).one()
-
     return render_template('post.html', post=post)
 
 @app.route('/all')
@@ -66,13 +67,19 @@ def submitpost():
     subtitle = request.form['subtitle']
     author = request.form['author']
     content = request.form['content']
+    image = request.files['image']
+    image.save('uploads/' + secure_filename(image.filename))
 
-    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now())
+    post = Blogpost(title=title, subtitle=subtitle, author=author, content=content, date_posted=datetime.now(), image=secure_filename(image.filename))
 
     db.session.add(post)
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory('uploads', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
